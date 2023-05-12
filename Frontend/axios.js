6276089ba162ff3374e98d69f5d6115f76e4ceae
Huggman <hugo.ericssonlfc@gmail.com>
2023-05-12 12:25:48 +0200

    hoppas detta funkar

--- Frontend/axios.js
+++ Frontend/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API_BASE_URL = axios.create({
-  baseURL: 'http://130.243.233.194:8000/',
+  baseURL: 'http://130.243.235.184:8000/',
 });
 
 export const API_REFRESH_URL = axios.create({
