332909cecda2812c00e857f70c91fa6d0963d339
lundbeergs <alma.lundberg@gmail.com>
2023-05-23 15:57:11 +0200

    Inget speciellt

--- Frontend/axios.js
+++ Frontend/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const API_BASE_URL = axios.create({
-  baseURL: 'http://192.168.10.117:8000/',
+  baseURL: 'http://192.168.68.115:8000/',
 });
 
 export const API_REFRESH_URL = axios.create({
-  baseURL: 'http://130.243.231.29:8000/',
+  baseURL: 'http://192.168.68.115:8000/',
 });
 
