0f92b8f742af27892c89fe43a251c9e58c7a6b6e
lundbeergs <alma.lundberg@gmail.com>
2023-05-26 10:51:47 +0200

    ändrade antal ord org bio

--- backend/userprofile/models.py
+++ backend/userprofile/models.py
@@ -29,7 +29,7 @@ class OrganizationProfile(models.Model):
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='organization_profile')
 	org_name = models.CharField(max_length=50, unique=False)
-	org_bio = models.CharField(max_length=50, unique=False)
+	org_bio = models.CharField(max_length=1000, unique=False)
 	# contact_email = models.EmailField(max_length=254, required = False, help_text ='Contact email')
 	# contact_phone = PhoneField(blank=True, null=True, required = False, help_text='Contact phone number')
 	
