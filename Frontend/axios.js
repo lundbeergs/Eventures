2958cf79b2f0397b9ce9c74a9f2bdd16653c683c
lundbeergs <alma.lundberg@gmail.com>
2023-05-26 09:56:50 +0200

    axios

--- Frontend/axios.js
+++ Frontend/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const API_BASE_URL = axios.create({
-  baseURL: 'http://192.168.0.6:8000/',
+  baseURL: 'http://192.168.10.197:8000/',
 });
 
 export const API_REFRESH_URL = axios.create({
-  baseURL: 'http://192.168.0.6:8000/',
+  baseURL: 'http://192.168.10.197:8000/',
 });
 
