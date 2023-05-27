ef941f794928bb487b36f14152684799c3a1dbd3
lthorsander <lovisa.thorsander@gmail.com>
2023-05-27 11:23:07 +0200

    fixar

--- Frontend/screens/myProfilePage.js
+++ Frontend/screens/myProfilePage.js
@@ -6,7 +6,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   ImageBackground,
-  Scrollview
+  ScrollView
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation, useRoute } from "@react-navigation/native";
